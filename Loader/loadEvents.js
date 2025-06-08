a4345755b805a432e870ce4e57cda3c249d87acf
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-08 19:37:50 +0200

    [FIX] loadEvents.js

--- Loader/loadEvents.js
+++ Loader/loadEvents.js
@@ -2,11 +2,10 @@ const fs = require("fs")
 
 module.exports = async bot => {
 
-  fs.readdirSync("./Commandes").filter(f => f.endsWith(".js")).forEach(async file => {
+  fs.readdirSync("./Events").filter(f => f.endsWith(".js")).forEach(async file => {
 
-    let command = require(`../Commandes/${file}`)
-    if (!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length - 3)} n'a pas de nom !`)
-    bot.commands.set(command.name, command)
-    console.log(`Commande ${file} chargée avec succès !`)
+    let event = require(`../Events/${file}`)
+    bot.on(file.split(".js").join(""), event.bind(null, bot))
+    console.log(`Event ${file} chargé avec succès !`)
   })
 }
