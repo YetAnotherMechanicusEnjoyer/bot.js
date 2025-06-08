386b0e049dbceb871dcd05e903aa5a74fdd13882
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-09 01:43:52 +0200

    [UPDATE] help.mjs
    [ADD] autocompletion
    [ADD] Additionnal informations for specified commands

--- src/cmds/help.mjs
+++ src/cmds/help.mjs
@@ -11,7 +11,7 @@ export const command = {
       type: "string",
       name: "cmd",
       description: "La commande à afficher",
-      autocomplete: false,
+      autocomplete: true,
       required: false
     }
   ],
@@ -24,30 +24,58 @@ export const command = {
         return interaction.reply(`Error: \`${args.getString("cmd")}\` is not a command.`);
     }
 
-    let categories = [];
-    bot.commands.forEach(command => {
-      if (!categories.includes(command.category)) categories.push(command.category);
-    })
-
-    let Embed = new Discord.EmbedBuilder()
-      .setColor(bot.color)
-      .setTitle(`Commands`)
-      .setAuthor({
-        name: interaction.member.user.username,
-        iconURL: interaction.member.user.displayAvatarURL({ dynmaic: true })
+    let Embed;
+    if (!cmd) {
+      let categories = [];
+      bot.commands.forEach(command => {
+        if (!categories.includes(command.category)) categories.push(command.category);
       })
-      .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
-      .setDescription(`Available commands: \`${bot.commands.size}\`\nAvailable categories: \`${categories.length}\``)
-      .setTimestamp()
-      .setFooter({
-        text: "/help",
-        iconURL: bot.user.displayAvatarURL({ dynamic: true })
-      });
 
-    categories.sort().forEach(async cat => {
-      let commands = bot.commands.filter(cmd => cmd.category === cat);
-      Embed.addFields({ name: `${cat}`, value: `${commands.map(cmd => `\`${cmd.name}\`: \`${cmd.description}\``).join("\n")}`, inline: true });
-    });
+      Embed = new Discord.EmbedBuilder()
+        .setColor(bot.color)
+        .setTitle(`Commands`)
+        .setAuthor({
+          name: interaction.member.user.username,
+          iconURL: interaction.member.user.displayAvatarURL({ dynmaic: true })
+        })
+        .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`Available commands: \`${bot.commands.size}\`\nAvailable categories: \`${categories.length}\``)
+        .setTimestamp()
+        .setFooter({
+          text: "/help",
+          iconURL: bot.user.displayAvatarURL({ dynamic: true })
+        });
+
+      categories.sort().forEach(async cat => {
+        let commands = bot.commands.filter(cmd => cmd.category === cat);
+        Embed.addFields({ name: `${cat}`, value: `${commands.map(cmd => `\`${cmd.name}\`: \`${cmd.description}\``).join("\n")}`, inline: true });
+      });
+    } else {
+      Embed = new Discord.EmbedBuilder()
+        .setColor(bot.color)
+        .setTitle(`\`${cmd.name}\` command`)
+        .setAuthor({
+          name: interaction.member.user.username,
+          iconURL: interaction.member.user.displayAvatarURL({ dynamic: true })
+        })
+        .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setFooter({
+          text: `/help ${cmd.name}`,
+          iconURL: bot.user.displayAvatarURL({ dynamic: true })
+        });
+
+      Embed.addFields({ name: `Name`, value: `\`${cmd.name}\``, inline: true });
+      Embed.addFields({ name: `Description`, value: `\`${cmd.description}\``, inline: false });
+      Embed.addFields({
+        name: `Requied Permission`,
+        value: `\`${typeof cmd.permission !== "bigint" ?
+          cmd.permission : new Discord.PermissionBitField(cmd.permission).toArray(false)}\``,
+        inline: true
+      });
+      Embed.addFields({ name: `Context`, value: `\`${cmd.context}\``, inline: true });
+      Embed.addFields({ name: `Category`, value: `\`${cmd.category}\``, inline: true });
+    }
 
     await interaction.reply({ embeds: [Embed] });
   }
